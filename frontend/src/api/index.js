a44d8f59f6869c0f9b742ad6bb1c880f3bf4ee3f
TheTai-devFE <thetai.dev@gmail.com>
2025-12-31 17:03:23 +0700

    return main url

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 // Sử dụng biến môi trường của Vite, nếu không có sẽ lấy mặc định là URL Render của bạn
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5001/api',
-  // baseURL: import.meta.env.VITE_API_URL || 'https://smartretail-warranty.onrender.com/api'
+  // baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5001/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://smartretail-warranty.onrender.com/api'
 });
 
 
