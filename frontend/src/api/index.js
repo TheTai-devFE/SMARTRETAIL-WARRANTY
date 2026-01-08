c2eae2c6671c55a76b396aa2c7f7d38b250aeda1
TheTai-devFE <thetai.dev@gmail.com>
2026-01-08 09:52:03 +0700

    add url

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || (import.meta.env.DEV ? 'http://localhost:5001/api' : 'https://smartretail-warranty.onrender.com/api')
+  // baseURL: import.meta.env.VITE_API_URL || (import.meta.env.DEV ? 'http://localhost:5001/api' : 'https://smartretail-warranty.onrender.com/api')
+  baseURL: 'https://smartretail-warranty.onrender.com/api'
 });
 
 export const hardwareApi = {
